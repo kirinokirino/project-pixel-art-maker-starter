739a7f39bc83ac2cac7b09d3f83b377a7e73ff53
2501 <kirinokirino2501@yahoo.com>
2017-12-18 04:22:37 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,26 @@
-// Select color input
-// Select size input
+var button = $("#sizePicker").find("input").last();
+button.on("click", makeGrid);
 
-// When size is submitted by the user, call makeGrid()
+var color = 0;
+$("#colorPicker").on("change", function(e){ color = e.target.value; });
 
-function makeGrid() {
-
-// Your code goes here!
-
+function makeColumns() {
+    for (var height = 0; height < $("#input_height").prop("value"); height++) {
+        $("#pixel_canvas").append('<tr class="row'+height+'"></tr>');
+    }
 }
+
+function makeGrid(e) {
+    e.preventDefault();
+    $("#pixel_canvas").text("");
+    makeColumns();
+    $("#pixel_canvas").find("tr").each(function(i){
+        for (var width = 0; width < $("#input_width").prop("value"); width++) {
+              $(this).append('<td class="col'+width+'"></td>');
+        }
+    });
+}
+
+$("#pixel_canvas").on("click", "td", function (e){
+    $(this).css("background-color", color);
+});
